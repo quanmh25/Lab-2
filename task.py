5b325139101ef8e5fce40808b934851588c12802
quanmh25 <maihoangquan250205@gmail.com>
2024-10-17 20:40:39 +0300

    Theory

---
+++ task.py
@@ -0,0 +1,15 @@
+
+# Вариант 2: До 2016 года
+
+import csv
+import json
+
+
+# Task 1:
+
+with open("books-en.csv", mode = 'r') as file:
+    lst = list(csv.DictReader(file))
+    for i in lst:
+        
+
+# Task 2:
--- test.py
+++ test.py
@@ -1,19 +1,49 @@
-import csv
+import csv          # Nếu muốn mở csv.py để xem trong Module CSV có gì thì Ấn Ctrl và click chuột
 import json 
 
+
 # File CSV (Comma Separated Values)
-# def csv_learn():                      
-#     csv.
+def csv_learn():                      
+    with open("memes_dataset.csv", mode = 'r', encoding = "utf8") as file:
+        # file.readlines()                     # in ra chậm hơn dùng for 
+        # for line in file:
+        #     print(line.strip())              # nếu chỉ print(line) nó sẽ in ra 1 khoảng trắng (tức là xuống dòng 2 lần)
+
+        csv_file = csv.DictReader(file)        # Đọc từng dòng của tệp csv sau đó chuyển đổi nó thành object dictionary 
+        lst = list(csv_file)                   # list(dictionary) 
+    # In kiểu này xấu, khó nhìn   
+        # print(lst)                            
+        # for i in lst:
+        #     print(i)
+
+    # Dùng json để in ra định dạng đẹp
+        print(json.dumps(lst, indent = 4))
+
+# Write CSV file:
+    header = ["id", "name", "age", "mark"]
+    students = ["407882", "quan", "19", "10"]
+
+    with open("test.csv", mode = 'w', newline = '') as write_file:         # Nếu k ghi newline = '', nó sẽ mặc định tự hiểu là newline = '\n' và sẽ tự xuống dòng
+        tmp = csv.writer(write_file)
+        tmp.writerow(header)
+        tmp.writerow(students)
+
 
 
 def json_learn():
-        
-    student = {                             # Dictionary        
+
+# Read JSON file
+    with open("out.json", mode = 'r') as json_file:
+        tmp = json.load(json_file)             # Đọc toàn bộ file JSON, sau đó chuyển nó thành đối tượng trong Python: List hoặc dictionary
+        print(json.dumps(tmp, indent = 4))     # Chuyển đổi tmp trở lại chuỗi JSON (string), với thụt lề 4 khoảng trắng 
+
+# Write JSON file      
+    student = {                                # Dictionary        
         "name" : "quan",
         "age" : "19"
     }
 
-    results = [                             # List[dictionary]
+    results = [                                # List[dictionary]
         {
             "name" : "quan",
             "mark" : "10"
@@ -30,12 +60,14 @@ def json_learn():
         "results" : results
     }
 
-    with open("test.json", mode = 'w') as file:   
-        json.dump(data, file, indent = 4)
+    with open("test.json", mode = 'w') as write_file:   
+        json.dump(data, write_file, indent = 4)
 # 1 file JSON chỉ chấp nhận 1 đối tượng JSON hợp lệ
 # JSON chỉ cho phép 1 cấu trúc chính ( 1 object {} hoặc 1 array [])
 # Nếu ghi json.dump() 2 lần, nó sẽ ghi 2 đối tượng k kết nối vào cùng 1 tệp -> gây ra tệp JSON k hợp lệ
 
 
+
 if __name__ == '__main__':
-    json_learn()
+    # json_learn()
+    csv_learn()
