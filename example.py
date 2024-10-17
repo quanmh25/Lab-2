cb561f5775a1da688b0a2826345416a74dcdafbd
quanmh25 <maihoangquan250205@gmail.com>
2024-10-17 20:42:54 +0300

    Theory

--- example.py
+++ example.py
@@ -3,23 +3,31 @@
 import csv
 import json
 
-
-DATASET_PATH = "memes_dataset.csv"
+# Đường dẫn đến file
+DATASET_PATH = "memes_dataset.csv"              
 OUT_PATH = "out.json"
 
+# Lấy tiêu đề của file csv
+def get_title(dataset):                   
+    dataset.seek(0)                                     # Đặt con trỏ về đầu tệp, để chắc chắn đọc từ đầu 
+    title = next(dataset)                               # Lấy dòng đầu tiên của file và cũng là tiêu đề                     
+    title = title.split(",")                            # Xét 1 dòng, sau đó tách các phần tử bị ngăn cách bởi dấu phẩy thành các cột
+    # title = list(title.split(","))                    # Khi gọi title.split() nó sẽ tự động trả về list nên k cần gán list ở đầu
+                                                        # title lúc này đang là list
+    title = [col.strip() for col in title]              # List comprehension để xóa bỏ khoảng trống ở đầu và cuỗi của mỗi tên cột trong title
 
-def get_title(dataset):
-    dataset.seek(0)
-    title = next(dataset)
-    title = title.split(",")
-    title = [col.strip() for col in title]
-
+    # title = [col.strip() for col in title.split(",")]       #Lệnh này có thể thay thế cho dòng 14 và 17 
     return title
 
 
-def get_object_alt(line, title):
-    reader = csv.DictReader([line], title, delimiter=',', quotechar='"')
-    result = next(reader)
+# Lấy 1 đối tượng từ 1 dòng dữ liệu Line trong csv và trả về dưới dạng dictionary
+def get_object_alt(line, title): 
+    # [line] biến line thành 1 list chỉ chứa 1 phần tử là dòng dữ liệu
+    # delimiter chỉ định dấu phẩy là ký tự phân cách giữa các tường
+    # quotechar chỉ định dấu " dùng để bao quanh các giá trị
+    
+    reader = csv.DictReader([line], title, delimiter=',', quotechar='"')         
+    result = next(reader)                                # Lấy đối tượng đầu tiên từ reader
     return result
 
 
@@ -70,7 +78,7 @@ def filter_year(dataset, title, year):
 
 
 if __name__ == "__main__":
-    with open(DATASET_PATH, encoding="utf-8") as dataset:
+    with open(DATASET_PATH, encoding="utf-8") as dataset:             # dataset đại diện cho file vừa mở
         title = get_title(dataset)
 
         res = filter_year(dataset, title, 1990)
